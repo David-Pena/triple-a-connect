c444c23df0f97bd02d199165c4bd5fefa199323d
David-Pena <dev.davidpe@gmail.com>
2024-06-28 00:30:20 -0700

    chore: base UI quick actions view

--- app/(tabs)/home.tsx
+++ app/(tabs)/home.tsx
@@ -1,70 +1,103 @@
-import { Image, StyleSheet, Platform } from 'react-native';
-
-import { HelloWave } from '@/components/HelloWave';
-import ParallaxScrollView from '@/components/ParallaxScrollView';
-import { ThemedText } from '@/components/ThemedText';
-import { ThemedView } from '@/components/ThemedView';
+import { useState } from "react";
+import { StyleSheet, View, Text, Image } from "react-native";
 
 export default function HomeScreen() {
+  const [actions, setActions] = useState([
+    {
+      id: 1,
+      title: "Emergency Pickup",
+      icon: "",
+    },
+    {
+      id: 2,
+      title: "Schedule a Screening",
+      icon: "",
+    },
+    {
+      id: 3,
+      title: "Report an incident",
+      icon: "",
+    },
+    {
+      id: 4,
+      title: "Contact Call Center",
+      icon: "",
+    },
+  ]);
+
   return (
-    <ParallaxScrollView
-      headerBackgroundColor={{ light: '#A1CEDC', dark: '#1D3D47' }}
-      headerImage={
-        <Image
-          source={require('@/assets/images/partial-react-logo.png')}
-          style={styles.reactLogo}
-        />
-      }>
-      <ThemedView style={styles.titleContainer}>
-        <ThemedText type="title">Welcome!</ThemedText>
-        <HelloWave />
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 1: Try it</ThemedText>
-        <ThemedText>
-          Edit <ThemedText type="defaultSemiBold">app/(tabs)/index.tsx</ThemedText> to see changes.
-          Press{' '}
-          <ThemedText type="defaultSemiBold">
-            {Platform.select({ ios: 'cmd + d', android: 'cmd + m' })}
-          </ThemedText>{' '}
-          to open developer tools.
-        </ThemedText>
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 2: Explore</ThemedText>
-        <ThemedText>
-          Tap the Explore tab to learn more about what's included in this starter app.
-        </ThemedText>
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 3: Get a fresh start</ThemedText>
-        <ThemedText>
-          When you're ready, run{' '}
-          <ThemedText type="defaultSemiBold">npm run reset-project</ThemedText> to get a fresh{' '}
-          <ThemedText type="defaultSemiBold">app</ThemedText> directory. This will move the current{' '}
-          <ThemedText type="defaultSemiBold">app</ThemedText> to{' '}
-          <ThemedText type="defaultSemiBold">app-example</ThemedText>.
-        </ThemedText>
-      </ThemedView>
-    </ParallaxScrollView>
+    <View style={styles.container}>
+      <View style={[styles.row, { height: 100 }]}>
+        <View style={styles.headerContainer}>
+          <Text style={styles.headerContainerText}>All you need, right here</Text>
+        </View>
+      </View>
+
+      <View style={styles.row}>
+        {actions.slice(0, 2).map((item) => (
+          <View key={item.id} style={styles.itemContainer}>
+            <Text style={styles.item}>{item.title}</Text>
+          </View>
+        ))}
+      </View>
+      <View style={styles.row}>
+        {actions.slice(2, 4).map((item) => (
+          <View key={item.id} style={styles.itemContainer}>
+            <Text style={styles.item}>{item.title}</Text>
+          </View>
+        ))}
+      </View>
+    </View>
   );
 }
 
 const styles = StyleSheet.create({
-  titleContainer: {
-    flexDirection: 'row',
-    alignItems: 'center',
-    gap: 8,
+  headerLogo: {
+    width: 100,
+    height: 100,
   },
-  stepContainer: {
-    gap: 8,
-    marginBottom: 8,
+  headerContainer: {
+    width: "100%",
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+    paddingHorizontal: 20,
   },
-  reactLogo: {
-    height: 178,
-    width: 290,
-    bottom: 0,
-    left: 0,
-    position: 'absolute',
+  headerContainerText: {
+    fontFamily: "ProtoNerd",
+    fontSize: 28,
+  },
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    padding: 20,
+
+    backgroundColor: "white",
+  },
+  row: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+    marginBottom: 20,
+    width: "100%", // Ensure items take full width of the container
+  },
+  itemContainer: {
+    flex: 1,
+
+    height: 130,
+    borderRadius: 25,
+
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "#F98C20",
+    padding: 20,
+    marginHorizontal: 10, // Adjust spacing between items
+  },
+  item: {
+    textAlign: "center",
+    color: "white",
+    fontFamily: "ProtoNerd",
+    fontSize: 15,
   },
 });
