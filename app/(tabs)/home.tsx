05e30f3bfc6c3c8f3afa71b7dae4ce4d00484f3a
David-Pena <dev.davidpe@gmail.com>
2024-06-29 11:42:15 -0700

    chore: update ui for home view

--- app/(tabs)/home.tsx
+++ app/(tabs)/home.tsx
@@ -1,44 +1,46 @@
 import { Colors } from "@/constants/Colors";
+import { Ionicons } from "@expo/vector-icons";
 import { useState } from "react";
 import { StyleSheet, View, Text, Image } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 
 export default function HomeScreen() {
-  const [actions, setActions] = useState([
-    {
-      id: 1,
-      title: "Emergency Pickup",
-      icon: "",
-    },
-    {
-      id: 2,
-      title: "Schedule a Screening",
-      icon: "",
-    },
-    {
-      id: 3,
-      title: "Report an incident",
-      icon: "",
-    },
-    {
-      id: 4,
-      title: "Contact Call Center",
-      icon: "",
-    },
-  ]);
-
   return (
     <SafeAreaView>
-      <View className="flex flex-row justify-between">
-        <View>
-          <Text>Hola</Text>
-        </View>
-        <View>
-          <Text>Mundo</Text>
+      <View className="flex justify-around">
+        <View className="gap-2">
+          <View className="flex justify-center items-center bg-[#F98C20] py-8">
+            <Ionicons name="person-outline" size={32} color={"white"} />
+            <Text className="text-lg text-white text-clip" style={styles.title}>
+              Emergency Pickup
+            </Text>
+          </View>
+          <View className="flex justify-center items-center bg-[#F98C20] py-8">
+            <Ionicons name="person-outline" size={32} color={"white"} />
+            <Text className="text-lg text-white" style={styles.title}>
+              Schedule a Screening
+            </Text>
+          </View>
+          <View className="flex justify-center items-center bg-[#F98C20] py-8">
+            <Ionicons name="person-outline" size={32} color={"white"} />
+            <Text className="text-lg text-white" style={styles.title}>
+              Report an incident
+            </Text>
+          </View>
+          <View className="flex justify-center items-center bg-[#F98C20] py-8">
+            <Ionicons name="person-outline" size={32} color={"white"} />
+            <Text className="text-lg text-white" style={styles.title}>
+              Contact Call Center
+            </Text>
+          </View>
         </View>
       </View>
     </SafeAreaView>
   );
 }
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  title: {
+    fontFamily: "SpaceMono",
+  },
+});
